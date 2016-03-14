897b564e7b6ba1c57587c508453cb748d2956200
Evghenii Gaburov <egaburov@dds.nl>
2016-03-14 12:59:25 -0700

    parse expression to rhs and lhs

--- testing/simple_gassert.cpp
+++ testing/simple_gassert.cpp
@@ -6,17 +6,16 @@
 int main(int argc, char *argv[])
 {
   ASSERT(argc <= 1);
-  auto& s= gassert::failed_on_host::get_instance();
+  auto &s = gassert::failed_on_host::get_instance();
   fprintf(stderr, " s.failed.size() = %d\n", (int)s.failed.size());
 
-  if (!s.empty())
+  for (auto &x : s.failed)
   {
-    auto &x = s.failed.front();
-  printf("x.filename= %s\n", x.filename.c_str());
-  printf("x.liennumber= %s\n", x.linenumber.c_str());
-  printf(" %s %s %s\n", x.lhs_expr.c_str(), x.op.c_str(), x.rhs_expr.c_str());
-  printf(" %s %s %s\n", x.lhs_value.c_str(), x.op.c_str(), x.rhs_value.c_str());
+    printf("x.filename= %s\n", x.filename.c_str());
+    printf("x.liennumber= %s\n", x.linenumber.c_str());
+    printf("FAILED: %s %s %s (%s %s %s)\n", x.lhs_expr.c_str(), x.op.c_str(),
+           x.rhs_expr.c_str(), x.lhs_value.c_str(), x.op.c_str(),
+           x.rhs_value.c_str());
   }
-  
 }
 
